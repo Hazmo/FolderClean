62c3e7e9ae0e35edd145d2e0b4223526ffb869da
Hazmo <harry_baker87@hotmail.com>
2013-09-01 20:28:17 +0100

    Allowed user to select folder to clean

--- FolderCleanUp.py
+++ FolderCleanUp.py
@@ -1,23 +1,28 @@
 import os
 import shutil
 
-dir_list = os.listdir('C:\Users\Maureen\Downloads')
+
+directory = raw_input("Please enter the directory you want to clean: ")
+dir_list = os.listdir(directory)
 unmoved_files = []
 
 for file in dir_list:
+	file_directory = directory + "\\" + file
 	if '.' in file:
 		if '.' in file[-3:]:
 			folder = file[-2:]
-			if not os.path.exists(folder):
-				os.mkdir(folder)
+			if not os.path.exists(folder_directory):
+				os.mkdir(folder_directory)
 			
 		else:
 			folder = file[-3:]
-			if not os.path.exists(folder):
-				os.mkdir(folder)
+			folder_directory = directory + "\\" + folder
+			if not os.path.exists(folder_directory):
+				os.mkdir(folder_directory)
 			
 		if not os.path.exists(folder + "\\" + file):
-			shutil.move(file, folder)
+			print file
+			shutil.move(file_directory, folder_directory)
 		else:
 			unmoved_files.append(file)
 
